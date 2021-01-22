251023fe4e7e9f2a11c12ef2d0d8b54afe7dae30
VankayalapatiSiri <1705838@kiit.ac.in>
2021-01-22 18:27:17 +0530

    JUnit Practice

---
+++ JUnitBasics/src/main/java/MathUtils.java
@@ -0,0 +1,17 @@
+public class MathUtils {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double circleArea(double radius) {
+        return 3.14 * radius * radius;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
